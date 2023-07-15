a4c9fe9548a1c446a833d2681b760c538541e50c
venkatesanram <venkat.becse@gmail.com>
2023-07-15 15:32:59 +0530

    Update streamlit_app.py
    Added picture and menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
-streamlit.title(" My Parents New Healthy Diner")
+streamlit.title(" My Mom's New Healthy Diner")
 
-streamlit.header('Breakfast menu')
-streamlit.text(' Omega 3 & Blueberry Oatmeal ')
-streamlit.text('Kale, Spinach & Rocket smoothie')
-streamlit.text('Hard-Bolied Ree-Range Egg')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
